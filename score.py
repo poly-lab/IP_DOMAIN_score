78ef234cd9342fc2e8943ae65e7174b972f3e37b
liebesu <lichunjiang42@gmail.com>
2016-08-10 17:32:02 +0800

    update
    update

--- score.py
+++ score.py
@@ -1,5 +1,7 @@
 import json
 import urllib
+import os
+import MySQLdb
 def domain_score(domain,apikey):
     url = 'https://www.virustotal.com/vtapi/v2/domain/report'
     parameters = {'domain': domain, 'apikey': apikey}
@@ -13,11 +15,31 @@ def ip_score(ip,apikey):
     response = urllib.urlopen('%s?%s' % (url, urllib.urlencode(parameters))).read()
     response_dict = json.loads(response)
     print response_dict
+def read_source(source_path):
+    sources=open(source_path).readlines()
+    print len(sources)
+def read_key(key_path):
+    lines=open(key_path,"r").readlines()
+    print len(lines)
+    for line in lines:
+        pass
 
+def insert_db(result):
+    db = MySQLdb.connect(host='localhost', db='ip_domain', user='root', passwd='polydata', port=3306,
+                         charset='utf8')
+    cursor = db.cursor() 
+    insert_sql='insert '
+     
+def control():
+    pass
 if __name__=="__main__":
+    key_path=os.path.join("key","key")
+    source_path=os.path.join("source","ip.txt")
     domain='ozgkraa.net'
     apikey='f938cb8607a8d497a789c47c8ad9fda85d92ad2c8bc1cc37b0d0a45cf408e4e2'
     ip='213.159.214.106'
-    domain_score(domain, apikey)
-    ip_score(ip, apikey)
+    read_key(key_path)
+    read_source(source_path)
+    #domain_score(domain, apikey)
+    #ip_score(ip, apikey)
     
